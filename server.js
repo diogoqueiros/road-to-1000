e8a523e2c6f4a663f44686332b649e5179900dd0
Diogo Queirós <diogoqueiros@outlook.com>
2025-02-23 02:31:26 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 import express from "express";
-import puppeteer from "puppeteer";
 import cors from "cors";
 
 const app = express();
@@ -8,40 +7,16 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/goals", async (req, res) => {
-  const cC = 5;
-  const k = s => s.split("").map(c => String.fromCharCode(c.charCodeAt() - cC)).join("");
-  const h = "myyux?44|||3rjxxn{xwtsfqit3fuu4uflj2ifyf4nsij}4uflj2ifyf3oxts";
-  const url = k(h);
-
   try {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
+    const cC = 5;
+    const k = s => s.split("").map(c => String.fromCharCode(c.charCodeAt() - cC)).join("");
+    const h = "myyux?44|||3rjxxn{xwtsfqit3fuu4uflj2ifyf4nsij}4uflj2ifyf3oxts";
 
-    await page.setRequestInterception(true);
-
-    page.on("request", (request) => {
-      if (request.url() === url) {
-        request.continue();
-      } else {
-        request.abort();
-      }
+    const response = await fetch(k(h));
+    const json = await response.json()
+    res.json({
+      goals: json?.[k("wjxzqy")]?.[k("ifyf")]?.[k("fqqXmjjyWtsfqitFqqYnrjXyfyx")]?.[k("jiljx")]?.[0]?.[k("stij")]?.[k("ltfqx")],
     });
-
-    await page.goto(url, { waitUntil: "domcontentloaded" });
-
-    const goals = await page.evaluate(() => {
-      return JSON.parse(document.body.innerText)
-        ?.result
-        ?.data
-        ?.allSheetRonaldoAllTimeStats
-        ?.edges?.[0]
-        ?.node
-        ?.goals || "Goals not found";
-    });
-
-    await page.close();
-    await browser.close();
-    res.json({ goals });
   } catch (error) {
     res.status(500).json({ error: "Error fetching goals", detail: error });
   }
